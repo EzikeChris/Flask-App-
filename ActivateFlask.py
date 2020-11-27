5a37368b67ab053d94f63627cf50e35ca771900e
ezikechris <christophermbe1009@gmail.com>
2020-11-27 03:23:20 +0100

    initial commit

---
+++ ActivateFlask.py
@@ -0,0 +1,47 @@
+
+# ACTIVATE FLASK #
+
+
+#ON LINUX / UNIX #
+
+
+# source venv/bin/activate # 
+
+# .VENV/BIN/ACTIVATE #
+
+
+
+
+# RUNNING FLASK IN VIRTUAL ENVIRONMENT #
+
+# Now that the virtual environment is activated, use the Python package manager pip to install Flask:
+
+ # pip install Flask #
+
+
+# ON WINDOWS OS # 
+
+# VENV/SCRIPTS/ACTIVATE #
+
+
+
+# RUN FLASK APP TO FEW SERVER # 
+
+# Unix Bash (Linux, Mac, etc.):
+
+ # $ export FLASK_APP=hello
+
+
+ # DEACTIVATE A VIRTUAL ENVIRONMENT#
+
+ # SIMPLY RUN THE COMMAND "DEACTIVATE" #
+$ flask run
+Windows CMD:
+
+> set FLASK_APP=hello
+> flask run
+Windows PowerShell:
+
+> $env:FLASK_APP = "hello"
+
+#
